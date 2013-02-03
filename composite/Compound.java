1b7d7a194f4f7b196dce90b12a2ff2a10ffff197
Heath Harrelson <heath.harrelson@gmail.com>
2013-02-02 18:45:51 -0800

    Change Compound Constructor
    Change the constructor for Compound to accept zero or more Statements to match
    the professor's intentions.
    
    Changes:
    
     * Compound.java: Constructor takes zero or more arguments.

--- composite/Compound.java
+++ composite/Compound.java
@@ -1,7 +1,7 @@
 /* Compound.java: Compound statements (begin .. end blocks) in the while language.
  *
  * Author:   Heath Harrelson <harrel2@pdx.edu>
- * Modified: 2013-01-28
+ * Modified: 2013-02-02
  *
  */
 
@@ -10,15 +10,12 @@ import java.util.*;
 public class Compound extends Statement {
 	private LinkedList<Statement> statements;
 
-	// The grammar shows blocks including at least two statements
-	Compound (Statement s1, Statement s2, Statement... more) {
-		statements = new LinkedList<Statement>();
+	// Blocks contain zero or more statements
+	Compound (Statement... statements) {
+		this.statements = new LinkedList<Statement>();
 
-		statements.add(s1);
-		statements.add(s2);
-
-		for (Statement s : more) {
-			statements.add(s);
+		for (Statement s : statements) {
+			this.statements.add(s);
 		}
 	}
 
