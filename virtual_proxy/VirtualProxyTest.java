947639c9b34ce5164e629adad6c112a4e969a089
Heath Harrelson <heath.harrelson@gmail.com>
2013-02-18 12:49:11 -0800

    Add Driver Program
    Adds a driver program that prints a menu and gets user input.
    
    Changes:
    
     * VirtualProxyTest.java

---
+++ virtual_proxy/VirtualProxyTest.java
@@ -0,0 +1,71 @@
+/* VirtualProxyTest.java: Driver program for Virtual Proxy demo.
+ *
+ * Author:   Heath Harrelson <harrel2@pdx.edu>
+ * Modified: 02/18/2013
+ *
+ */
+
+import java.io.*;
+
+class VirtualProxyTest {
+	private static String status = "Enter your selection below.";
+
+	public static void main (String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String optionSelected = "";
+
+		boolean userQuit = false;
+		while (!userQuit) {
+			printMenu();
+			printStatus();
+			printPrompt();
+
+			try {
+				optionSelected = in.readLine();
+			} catch (IOException ioe) { }
+			userQuit = doSelection(optionSelected);
+		}
+	}
+
+	static void printMenu () {
+		TerminalIO.clearScreen();
+		TerminalIO.seekTo(10, 10);
+
+		TerminalIO.printHighlighted("Select an Option:");
+	}
+
+	static void printPrompt() {
+		TerminalIO.seekTo(TerminalIO.screenHeight(), 1);
+		TerminalIO.printHighlighted("Selection: ");
+	}
+
+	static void setStatus (String statusString) {
+		status = statusString;
+	}
+
+	static void printStatus () {
+		TerminalIO.seekTo(TerminalIO.screenHeight() - 1, 1);
+		TerminalIO.printReverseVideo(TerminalIO.padStringToLineLength(" " + status));
+	}
+
+	static boolean doSelection (String selection) {
+		if (selection.matches("((^[qQ]$)|^[Qq]uit$)"))
+			return true;
+
+		switch (selection) {
+			case "1":
+				setStatus("Did fast thing. Please select again.");
+				break;
+			case "2":
+				setStatus("Did slow thing. Please select again.");
+				break;
+			case "3":
+				setStatus("Should crash here.");
+				break;
+			default:
+				setStatus("Invalid option \"" + selection + "\". Please select again.");
+		}
+
+		return false;
+	}
+}
