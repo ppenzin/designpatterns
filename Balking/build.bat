bdf76ea4c52bdfd07ce53621ad733a214ad6f93b
Peter Penzin <p.o.penzin@gmail.com>
2013-03-14 10:56:51 -0700

    Added Balking. Very simple example, based on Professor's solution.

---
+++ Balking/build.bat
@@ -0,0 +1,5 @@
+rem Nice little script to build all that Java stuff 
+
+pushd src
+javac.exe -d ../build Main.java
+popd
---
+++ Balking/run.bat
@@ -0,0 +1,4 @@
+rem Nice little script to run our Java applet
+
+appletviewer.exe Bubble.htm
+
